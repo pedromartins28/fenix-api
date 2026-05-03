e1a529cb534abf15626c99410edaf9f44f91ecb7
pedromartins28 <pedromartins28023@gmail.com>
2026-05-03 19:36:04 -0300

    docs: add swagger doc to laravel api

--- api/routes/web.php
+++ api/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', function () {
+    return view('swagger');
+});
+
+Route::get('/docs/', function () {
+    return view('swagger');
+});
